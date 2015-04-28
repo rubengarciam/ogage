f45504e533b0d9d6f8a847a24261f7e0d8065745
Nishad Bakshi <nishadbakshi@gmail.com>
2015-04-28 03:49:12 +0000

    API with login and db placeholder

---
+++ api/server.js
@@ -0,0 +1,31 @@
+var port = process.env.PORT;
+
+
+// Include Express
+var express = require('express');
+
+var bodyParser = require('body-parser');
+
+//include user controller
+var user = require('controller/user');
+
+// Create a new Express application
+var app = express();
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Add a basic route – index page
+
+
+app.get('/', function (req, res) {
+    res.json({message: 'hello'});
+});
+
+
+app.post('/api/login', user.login);
+
+
+// Bind to a port
+app.listen(port);
+console.log('Worker  running!');

36fdac11aec3349e4302a9714bbca30a87b451df
Sean El-Sayed <seanelsayed@gmail.com>
2015-05-25 18:45:30 +1000

    Ionic'ised the templates

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -3,17 +3,32 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('ogage', ['ionic'])
+var app = angular.module('ogage', ['ionic'])
+.config(function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+		.state('login', {
+			url: '/login',
+			templateUrl: 'login.html',
+			controller: 'LoginController'
+		})
+		.state('profile', {
+			url: '/profile',
+			templateUrl: 'profile.html',
+			controller: 'ProfileController'
+		})
+		.state('schedule', {
+			url: "/schedule",
+			templateUrl: "schedule.html",
+			controller: "ScheduleController"
+		});
+	$urlRouterProvider.otherwise('/login');
+});
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+app.controller('LoginController', function($scope) {
+	console.log("Hello World!");
+	
+	$scope.login = function(){
+		console.log("login");
+	}
+    
+});

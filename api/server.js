26bf853b1933b552cd10724598c2ff7138491d40
unknown <nbakshi@NBAKSHI-AU.au.oracle.com>
2015-06-09 11:48:24 +1000

    sample db code!

--- api/server.js
+++ api/server.js
@@ -19,7 +19,6 @@ exports.secret=secret;
 
 // Create a new Express application
 var app = express();
-
 // parse application/json
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,7 +31,6 @@ app.use(expressJwt({ secret: secret}).unless({path: ['/api/login']}));
 app.get('/', function (req, res) {
     res.json({message: 'hello'});
 });
-
 
 app.post('/api/login', user.login);
 
@@ -50,3 +48,33 @@ app.get('/api/restricted', function (req, res) {
 // Bind to a port
 app.listen(port);
 console.log('Worker  running!');
+
+var oracledb = require('oracledb');
+
+oracledb.getConnection(
+    {
+      user          : "Yoga",
+      password      : "Welcome1#",
+      connectString : "opc.opcau.com:1521/yogapdb.auoracle84096.oraclecloud.internal"
+    },
+    function(err, connection)
+    {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+      connection.execute(
+          "SELECT * "
+          + "FROM Users "
+          //+ "WHERE department_id = :did"
+          ,
+          //[180],
+          function(err, result)
+          {
+            if (err) {
+              console.error(err.message);
+              return;
+            }
+            console.log(result.rows);
+          });
+    });
